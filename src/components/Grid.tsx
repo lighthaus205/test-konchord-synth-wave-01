73ec79335d909ae689e862990dae008d5c877209
kla <konstantinlackner@gmail.com>
2023-10-05 15:25:13 +0200

    Create Walls

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,5 +1,5 @@
 import { useLoader } from "@react-three/fiber"
-import { RigidBody } from "@react-three/rapier"
+import { CuboidCollider, RigidBody } from "@react-three/rapier"
 import { KOLORS } from "~/utils/globals"
 import { TextureLoader } from 'three/src/loaders/TextureLoader'
 
@@ -7,7 +7,31 @@ import { TextureLoader } from 'three/src/loaders/TextureLoader'
 export default function Grid() {
   const gridMap = useLoader(TextureLoader, 'gridmap.png')
   return <>
-    <RigidBody>
+    <RigidBody type="fixed">
+      <CuboidCollider
+        args={[0.1, 2, 8]}
+        position={[8, 2, 0]}
+      />
+    </RigidBody>
+    <RigidBody type="fixed">
+      <CuboidCollider
+        args={[0.1, 2, 8]}
+        position={[-8, 2, 0]}
+      />
+    </RigidBody>
+    <RigidBody type="fixed">
+      <CuboidCollider
+        args={[8, 2, 0.1]}
+        position={[0, 2, 8]}
+      />
+    </RigidBody>
+    <RigidBody type="fixed">
+      <CuboidCollider
+        args={[8, 2, 0.1]}
+        position={[0, 2, -8]}
+      />
+    </RigidBody>
+    <RigidBody type="fixed">
       <mesh
         rotation={[-Math.PI * 0.5, 0, 0]}
         scale={1}
