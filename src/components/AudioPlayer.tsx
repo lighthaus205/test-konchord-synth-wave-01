8b46246b56b4bb6482a20052e461ff4a01a380fc
kla <konstantinlackner@gmail.com>
2023-12-17 12:40:18 +0100

    Enter fullscreen

--- src/components/AudioPlayer.tsx
+++ src/components/AudioPlayer.tsx
@@ -1,11 +1,31 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import * as Tone from 'tone'
 
 const player = new Tone.Player("/audio/Down_The_Rabbit_Hole_v3.mp3").toDestination();
 
-export default function AudioPlayer() {
+export default function AudioPlayer({
+  isFullscreen
+}: {
+  isFullscreen: Boolean
+}) {
   console.log('render AudioPlayer...')
   const [play, setPlay] = useState(false);
+
+  const enterFullscreen = () => {
+    try {
+      document.body.requestFullscreen()
+    } catch {
+      console.error('Cannot enter fullscreen')
+    }
+  }
+  const exitFullscreen = () => {
+    try {
+      document.exitFullscreen()
+    } catch {
+      console.error('Cannot enter fullscreen')
+    }
+    
+  }
 
   const startMusic = () => {
     player.start();
@@ -22,6 +42,9 @@ export default function AudioPlayer() {
       <div className='playMusicWrapper'>
         <div className='playMusic' onClick={play === true ? muteMusic : startMusic}>
           {play === true ? "Stop music" : "Play music"}
+        </div>
+        <div className='toggleFullscreen' onClick={isFullscreen === true ? exitFullscreen : enterFullscreen}>
+          {isFullscreen === true ? "Exit fullscreen" : "Enter fullscreen"}
         </div>
       </div>
     </div>
